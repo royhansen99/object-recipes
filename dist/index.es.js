4ff9d2164d321369c59e4fcdd7bab3fa101bc462
royhansen99 <roy@royhansen.no>
2025-10-05 00:53:24 +0200

    feat: Forgot to rebuild dist-files after previous commit..

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,33 +1,46 @@
-function y(e) {
+function a(e) {
   if (e === null || typeof e != "object") return e;
   const t = Array.isArray(e) ? [] : {};
-  for (const i in e)
-    Object.prototype.hasOwnProperty.call(e, i) && (t[i] = y(e[i]));
+  for (const r in e)
+    Object.prototype.hasOwnProperty.call(e, r) && (t[r] = a(e[r]));
   return t;
 }
-const a = (e, t, i) => {
-  const s = t.replace(/(\[[^\[\]]*\])/g, ".$1").split("."), c = { ...e };
-  let n = c;
-  for (let l = 0; l < s.length; l++) {
-    let r = s[l];
-    if (r.slice(0, 1) === "[" && (r = r.slice(1, -1)), n === null || typeof n != "object")
+const c = (e, t, r, i = !1) => {
+  let n = e;
+  for (let l = 0; l < t.length; l++) {
+    const s = t[l];
+    if (n === null || typeof n != "object")
       throw new Error(
         "One or more path levels are not valid. The entire nested structure you specified must be spreadable down to (but not including) the last item."
       );
-    const u = n[r];
-    l === s.length - 1 ? n[r] = i : u !== null && typeof u == "object" && (n[r] = Array.isArray(n[r]) ? [...n[r]] : { ...n[r] }), n = n[r];
+    const u = n[s];
+    if (l === t.length - 1)
+      if (i) {
+        if (Object.is(u, r))
+          return !0;
+      } else return n[s] = r, !0;
+    else !i && u !== null && typeof u == "object" && (n[s] = Array.isArray(n[s]) ? [...n[s]] : { ...n[s] });
+    n = n[s];
   }
-  return c;
+  return !1;
+}, h = (e, t, r) => {
+  const i = t.replace(/\[([^\[\]]*)\]/g, ".$1").split(".");
+  if (c(e, i, r, !0)) return e;
+  const n = { ...e };
+  return c(n, i, r), n;
 };
 class o {
   constructor(t) {
     this.entity = t;
   }
   set(t) {
-    return new o({ ...this.entity, ...t });
+    return Object.keys(t).filter(
+      (i) => !Object.is(t[i], this.entity[i])
+    ).length !== 0 ? new o({ ...this.entity, ...t }) : this;
   }
-  setPath(t, i) {
-    return new o(a(this.entity, t, i));
+  setPath(t, r) {
+    const i = h(this.entity, t, r);
+    return Object.is(i, this.entity) ? this : new o(i);
   }
   recipe(t) {
     return t(this);
@@ -36,13 +49,13 @@ class o {
     return this.entity;
   }
   getClone() {
-    return y(this.entity);
+    return a(this.entity);
   }
 }
-function h(e) {
+function y(e) {
   return new o(e);
 }
 export {
   o as EntityClass,
-  h as entity
+  y as entity
 };
